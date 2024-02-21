6f3d3ec4d3bd0c8ced30db215dbe487f66428c2b
spaaacy <90762160+spaaacy@users.noreply.github.com>
2024-02-21 08:23:04 -0500

    created basic navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
+import NavBar from "@/components/NavBar";
+
 export default function Home() {
   return (
     <main className="">
+      <NavBar />
       <h1>Hello world!</h1>
     </main>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,11 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        blueprimary: "#318CE7",
+      },
+      fontFamily: {
+        roboto: ["Roboto", "sans-serif"],
       },
     },
   },

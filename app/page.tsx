2c1bb52dccb0d4a5a831ba5ecf8bad4589e3a87a
spaaacy <90762160+spaaacy@users.noreply.github.com>
2024-02-21 14:29:04 -0500

    Begun designing basic layout

--- app/page.tsx
+++ app/page.tsx
@@ -4,23 +4,17 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="min-h-screen flex flex-col">
+    <main className="flex flex-col flex-1">
       <NavBar />
-      <div className="flex flex-col flex-1 justify-center">
-        <h1 className="mx-auto text-6xl font-bold mt-8">
+      <div className="flex flex-col flex-1 justify-center px-4">
+        <h1 className="mx-auto text-6xl font-bold mt-8 text-center">
           Connecting developers with employers.
         </h1>
         <div className="flex justify-center items-center gap-4 mt-6">
-          <Link
-            href={"/business"}
-            className="border border-gray-800 text-lg font-normal rounded-lg py-2 px-4"
-          >
+          <Link href={"/business"} className="outline-button">
             Businesses
           </Link>
-          <Link
-            href={"/individual"}
-            className="border border-gray-800 text-lg font-normal rounded-lg py-2 px-4"
-          >
+          <Link href={"/individual"} className="outline-button">
             Individuals
           </Link>
         </div>

b81324251d320496c89ec97ef2fdab5b4d533fc4
spaaacy <90762160+spaaacy@users.noreply.github.com>
2024-02-21 18:22:34 -0500

    Finished basic layout for CreateJob

--- components/business/CreateJob.tsx
+++ components/business/CreateJob.tsx
@@ -10,20 +10,33 @@ export const CreateJob = () => {
       description,
       minimumPay,
       maximumPay,
+      position,
+      remote,
     });
+  };
+
+  const handlePosition = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setPosition(e.target.value);
+  };
+
+  const handleRemote = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setRemote(e.target.value);
   };
 
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const [minimumPay, setMinimumPay] = useState("");
   const [maximumPay, setMaximumPay] = useState("");
+  const [position, setPosition] = useState("junior");
+  const [remote, setRemote] = useState("onsite");
+  const [location, setLocation] = useState("");
 
   return (
     <form className="flex flex-col gap-2 w-[720px] items-start" onSubmit={(e) => handleSubmit(e)}>
       <h1 className="text-4xl font-bold mb-4">Create new job listing</h1>
       <input
         onChange={(e) => setTitle(e.target.value)}
-        className="input-box  w-full"
+        className="input-box w-full"
         placeholder="Job Title"
         type="text"
       />
@@ -32,22 +45,52 @@ export const CreateJob = () => {
         className="input-box resize-none w-full"
         placeholder="Job Description"
       />
-      <div className="flex gap-4 items-center">
+      <div className="flex gap-2 w-full">
+        <input
+          onChange={(e) => setLocation(e.target.value)}
+          className="input-box w-full"
+          placeholder="Location"
+          type="text"
+        />
+        <select
+          defaultValue={"onsite"}
+          className="rounded-xl px-4 py-2 border-2"
+          value={remote}
+          onChange={handleRemote}
+          name="remote"
+        >
+          <option value={"onsite"}>On-Site</option>
+          <option value={"remote"}>Remote</option>
+        </select>
+      </div>
+      <div className="flex items-center w-full gap-2">
+        <p className="font-bold">$</p>
         <input
           onChange={(e) => setMinimumPay(e.target.value)}
-          className="input-box"
+          className="input-box w-full"
           placeholder="Minimum Pay"
           type="text"
         />
-        <p>-</p>
+        <p className="font-bold  mx-2">-</p>
+        <p className="font-bold">$</p>
         <input
           onChange={(e) => setMaximumPay(e.target.value)}
-          className="input-box"
+          className="input-box w-full"
           placeholder="Maximum Pay"
           type="text"
         />
+        <select
+          defaultValue={"junior"}
+          className="rounded-xl px-4 py-2 border-2"
+          value={position}
+          onChange={handlePosition}
+          name="position"
+        >
+          <option value={"intern"}>Intern</option>
+          <option value={"junior"}>Junior</option>
+          <option value={"senior"}>Senior</option>
+        </select>
       </div>
-      {/* TODO: Add dropdown for type of job */}
       <button className="outline-button self-end" type="submit">
         Submit
       </button>

851d4e52c0a7fd82bac4ba34e87f7dca412c3d42
Kenneth Camacho <Kennethcxv@gmail.com>
2024-02-21 22:13:08 -0500

    Deleted the console.log

--- app/(dashboard)/admin/page.tsx
+++ app/(dashboard)/admin/page.tsx
@@ -1,12 +1,10 @@
-import { authOptions } from "@/lib/auth"
-import { getServerSession } from "next-auth"
+import { authOptions } from "@/lib/auth";
+import { getServerSession } from "next-auth";
 
-const page = async ()  => {
-    const session = await getServerSession(authOptions)
-    console.log(session)
-    return (
-      <div>Welcome to admin</div>
-    )
-  }
-  
-  export default page
+const page = async () => {
+  const session = await getServerSession(authOptions);
+
+  return <div>Welcome to admin </div>;
+};
+
+export default page;

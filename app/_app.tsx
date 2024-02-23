408981a5acc775227c89b742f7547c2f13b151a8
Kenneth Camacho <Kennethcxv@gmail.com>
2024-02-22 22:51:31 -0500

    Added Profile on sign in

---
+++ app/_app.tsx
@@ -0,0 +1,12 @@
+// In your _app.tsx file
+import { SessionProvider } from "next-auth/react";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <SessionProvider session={pageProps.session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
+}
+
+export default MyApp;
--- components/Individual/registration/UserAccountNav.tsx
+++ components/Individual/registration/UserAccountNav.tsx
@@ -1,21 +1,50 @@
 "use client";
-
-import { Button } from "@/components/ui/button";
-import { signOut } from "next-auth/react";
+import { useState } from "react";
+import { signOut, useSession } from "next-auth/react";
 
 const UserAccountNav = () => {
+  const { data: session } = useSession();
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => setDropdownOpen(!dropdownOpen);
+
   return (
-    <Button
-      onClick={() =>
-        signOut({
-          redirect: true,
-          callbackUrl: `${window.location.origin}/SignIn`,
-        })
-      }
-      variant={"destructive"}
-    >
-      Sign Out
-    </Button>
+    <div className="relative">
+      {session && (
+        <>
+          <img
+            onClick={toggleDropdown}
+            src={session.user.image || "/Default_pfp.png"} // Placeholder for default profile picture
+            alt="Profile"
+            className="cursor-pointer rounded-full w-10 h-10"
+          />
+          {dropdownOpen && (
+            <div className="absolute right-0 mt-2 py-2 w-48 bg-white rounded-md shadow-xl z-20">
+              <a
+                href="/account"
+                className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+              >
+                Account
+              </a>
+              <a
+                href="/settings"
+                className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+              >
+                Settings
+              </a>
+              <button
+                onClick={() =>
+                  signOut({ redirect: true, callbackUrl: "/SignIn" })
+                }
+                className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 w-full text-left"
+              >
+                Sign Out
+              </button>
+            </div>
+          )}
+        </>
+      )}
+    </div>
   );
 };
 

43a37b23c182c337b00af7cceb46cbc29646ed4f
spaaacy <90762160+spaaacy@users.noreply.github.com>
2024-02-23 08:31:44 -0500

    Fixed business error, removed getServerSession from navbar

--- components/Individual/registration/UserAccountNav.tsx
+++ components/Individual/registration/UserAccountNav.tsx
@@ -1,10 +1,11 @@
 "use client";
+
 import { useState } from "react";
-import { signIn, signOut, useSession } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
 
 const UserAccountNav = () => {
-  const { data: session } = useSession();
   const [dropdownOpen, setDropdownOpen] = useState(false);
+  const { data: session } = useSession();
 
   const toggleDropdown = () => setDropdownOpen(!dropdownOpen);
 
@@ -15,46 +16,27 @@ const UserAccountNav = () => {
 
   return (
     <div className="relative">
-      {session ? (
-        // User is signed in
-        <>
-          <img
-            onClick={toggleDropdown}
-            src={session.user.image || "/Default_pfp.png"}
-            alt="Profile"
-            className="cursor-pointer rounded-full w-10 h-10"
-          />
-          {dropdownOpen && (
-            <div className="absolute right-0 mt-2 py-2 w-48 bg-white rounded-md shadow-xl z-20">
-              <a
-                href="/profile"
-                className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-              >
-                Account
-              </a>
-              <a
-                href="/settings"
-                className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-              >
-                Settings
-              </a>
-              <button
-                onClick={handleSignOut}
-                className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 w-full text-left"
-              >
-                Sign Out
-              </button>
-            </div>
-          )}
-        </>
-      ) : (
-        // User is not signed in
-        <button
-          onClick={() => signIn()}
-          className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 w-full text-left"
-        >
-          Sign In
-        </button>
+      <img
+        onClick={toggleDropdown}
+        src={session.user.image || "/Default_pfp.png"}
+        alt="Profile"
+        className="cursor-pointer rounded-full w-10 h-10"
+      />
+      {dropdownOpen && (
+        <div className="absolute right-0 mt-2 py-2 w-48 bg-white rounded-md shadow-xl z-20">
+          <a href="/profile" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
+            Account
+          </a>
+          <a href="/settings" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
+            Settings
+          </a>
+          <button
+            onClick={handleSignOut}
+            className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 w-full text-left"
+          >
+            Sign Out
+          </button>
+        </div>
       )}
     </div>
   );

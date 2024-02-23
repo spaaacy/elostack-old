85d2693cfb938c43569857a65cd7289d502a5c20
Kenneth Camacho <Kennethcxv@gmail.com>
2024-02-23 09:54:00 -0500

    Finished with the editprofile page

---
+++ app/(profile)/editprofile/page.tsx
@@ -0,0 +1,12 @@
+import AccountPage from "@/components/Individual/profile/EditProfilePage";
+import NavBar from "@/components/NavBar";
+const Accounts = () => (
+  <>
+    <NavBar />
+    <div className="flex h-screen">
+      <AccountPage />
+    </div>
+  </>
+);
+
+export default Accounts;

e1d523df2c2367d6af8b306bca84d2eb1b57d786
spaaacy <90762160+spaaacy@users.noreply.github.com>
2024-02-26 20:15:02 -0500

    Got /apply page to work with backend. job-listing page displays actual job based on backend too. /apply page is search enabled

---
+++ app/api/job-listing/route.tsx
@@ -0,0 +1,28 @@
+import { supabase } from "@/lib/supabase";
+import { useSearchParams } from "next/navigation";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest, res: NextResponse) {
+  try {
+    const params = await req.json();
+    const nonEmptyParams = {};
+    for (const key in params) {
+      if (params[key] !== "") {
+        nonEmptyParams[key] = params[key];
+      }
+    }
+    console.log(nonEmptyParams);
+    const { data, error } = await supabase
+      .from("job_listing")
+      .select()
+      .match({
+        active: true,
+        ...nonEmptyParams,
+      });
+    if (error) throw error;
+    return NextResponse.json({ data }, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ error }, { status: 500 });
+  }
+}

6c938776292e9201d1ca889f6bd5106774b5b071
spaaacy <90762160+spaaacy@users.noreply.github.com>
2024-03-04 14:21:19 -0500

    Admin: Only displays pending interview requests

--- components/admin/admin.tsx
+++ components/admin/admin.tsx
@@ -19,12 +19,13 @@ const AdminDashboard: React.FC = () => {
   }, []);
 
   const fetchRequests = async () => {
-    const response = await fetch("/api/interview-request/", {
+    const response = await fetch("/api/interview-request?pending=true", {
       method: "GET",
     });
     if (response.status === 200) {
       const results = await response.json();
       setInterviewRequests(results.requests);
+      console.log(results);
     } else {
       console.error("Error fetching interview requests!");
     }
@@ -65,13 +66,14 @@ const AdminDashboard: React.FC = () => {
         </section>
 
         <section className="bg-center p-8 rounded-lg shadow-lg">
-          <h2 className="text-3xl font-bold text-blueprimary">Interview Requests</h2>
+          <h2 className="text-3xl font-bold text-blueprimary">Pending Interview Requests</h2>
           <table className="w-full table-auto text-sm">
             <thead>
               <tr>
                 <th className="px-4 py-2">Request ID</th>
                 <th className="px-4 py-2">Creation Date</th>
                 <th className="px-4 py-2">Candidate Name</th>
+                <th className="px-4 py-2">Company</th>
                 <th className="px-4 py-2">Position</th>
                 <th className="px-4 py-2">Technical Length</th>
                 <th className="px-4 py-2">Behavioral Length</th>
@@ -86,13 +88,14 @@ const AdminDashboard: React.FC = () => {
                       <td className="border px-4 py-2">{request.id}</td>
                       <td className="border px-4 py-2">{formatDate(request.created_at)}</td>
                       <td className="border px-4 py-2">{request.individual_name}</td>
+                      <td className="border px-4 py-2">{request.business.name}</td>
                       <td className="border px-4 py-2 capitalize">{request.position}</td>
                       <td className="border px-4 py-2">{request.technical_length}</td>
                       <td className="border px-4 py-2">{request.behavioral_length}</td>
                     </tr>
                     <tr>
                       <td className="border px-4 py-2 font-extrabold">Description</td>
-                      <td colspan="5" className="border px-4 py-2">
+                      <td colspan="6" className="border px-4 py-2">
                         {request.description}
                       </td>
                     </tr>
@@ -140,7 +143,7 @@ const AdminDashboard: React.FC = () => {
               {errors.feedback && <p>This field is required</p>}
             </div>
             <div>
-              <label className="block font-semibold">Video</label>
+              <label className="block font-semibold">Video URL</label>
               <input
                 type="text"
                 {...register("video_url", { required: true })}

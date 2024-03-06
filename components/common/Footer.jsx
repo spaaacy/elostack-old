08da7577016aae7cccd82021ae9bf1ae80bfe8b0
spaaacy <90762160+spaaacy@users.noreply.github.com>
2024-03-06 15:27:51 -0500

    Added tc to footer

--- components/common/Footer.jsx
+++ components/common/Footer.jsx
@@ -1,7 +1,14 @@
+import Link from "next/link";
+
 const Footer = () => {
   return (
     <footer className="mt-auto text-center py-2 text-sm text-gray-400 font-light">
       Copyright © 2024 EloStack, Inc. All Rights Reserved.
+      <div>
+        <Link href="/terms-and-conditions.html" className="hover:underline">
+          Terms & Conditions.
+        </Link>
+      </div>
     </footer>
   );
 };

5aa3c66f62f19d7daf1232ea493a5093431184fe
spaaacy <90762160+spaaacy@users.noreply.github.com>
2024-03-24 13:14:01 -0400

    Added log to user

--- app/admin/page.jsx
+++ app/admin/page.jsx
@@ -1,6 +1,5 @@
 "use client";
 
-export const dynamic = "force-dynamic";
 import Loader from "@/components/common/Loader";
 import NavBar from "@/components/common/NavBar";
 import { UserContext } from "@/context/UserContext";

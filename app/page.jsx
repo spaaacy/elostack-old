d6827a1f37b8d059c08007e9279a50f5187ca37a
spaaacy <90762160+spaaacy@users.noreply.github.com>
2024-03-30 12:34:34 -0400

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -25,7 +25,7 @@ const Home = () => {
         {/* <PartneredCompanies /> */}
         <Benefits />
         <Discount />
-        <HowItWorks />
+        {/* <HowItWorks /> */}
         <FAQ />
       </main>
       <Footer />

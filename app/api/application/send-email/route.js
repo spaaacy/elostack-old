854ad4df8940dca55eb5d96878a13aaed810cb73
spaaacy <90762160+spaaacy@users.noreply.github.com>
2024-03-31 22:29:00 -0400

    Working implementation for checking for user options matches lead company, state, city

--- app/api/application/send-email/route.js
+++ app/api/application/send-email/route.js
@@ -48,44 +48,93 @@ export async function POST(req, res) {
 
     results = await supabase.from("subscribed_user").select("*, user(*, individual(*))");
 
-    // console.log(results.data);
     results.data.forEach(async (user) => {
-      if (!user.refresh_token || !user.access_token) throw Error("Refresh token and/or access token not found!");
+      try {
+        if (!user.active) return;
+        if (!user.refresh_token || !user.access_token) throw Error("Refresh token and/or access token not found!");
 
-      const transporter = nodeMailer.createTransport({
-        service: "Gmail",
-        host: "smtp.gmail.com",
-        port: 465,
-        secure: true,
-        auth: {
-          type: "OAuth2",
-          clientId: process.env.GOOGLE_OAUTH_CLIENT_ID,
-          clientSecret: process.env.GOOGLE_OAUTH_CLIENT_SECRET,
-          user: user.user.email,
-          refreshToken: user.refresh_token,
-          accessToken: user.access_token,
-        },
-      });
+        const transporter = nodeMailer.createTransport({
+          service: "Gmail",
+          host: "smtp.gmail.com",
+          port: 465,
+          secure: true,
+          auth: {
+            type: "OAuth2",
+            clientId: process.env.GOOGLE_OAUTH_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_OAUTH_CLIENT_SECRET,
+            user: user.user.email,
+            refreshToken: user.refresh_token,
+            accessToken: user.access_token,
+          },
+        });
 
-      let chosenLead,
-        unique = false;
-      while (!unique) {
-        chosenLead = leads[Math.floor(Math.random() * leads.length)];
-        unique = true;
+        user.options.companies = user.options.companies.map((company) => company.toLowerCase());
+        user.options.states = user.options.states.map((state) => state.toLowerCase());
+        user.options.cities = user.options.cities.map((city) => {
+          return { city: city.city.toLowerCase(), state: city.state.toLowerCase() };
+        });
+
+        let chosenLead,
+          found = false,
+          count = 0,
+          visitedLeads = [];
+        while (!found) {
+          const index = Math.floor(Math.random() * leads.length);
+          if (visitedLeads.includes(index)) continue;
+          visitedLeads.push(index);
+          if (++count === 10) {
+            // Exit after ten attempts made to find match
+            console.log("No matches found!");
+            break;
+          }
+          chosenLead = leads[index];
+
+          if (
+            (user.options.companies.length > 0 &&
+              user.options.companies.includes(chosenLead.organization_name.toLowerCase())) || // User specified a company & matches lead's company
+            user.options.companies.length === 0 // Or user didn't specify any companies
+          ) {
+            if (user.options.cities.length === 0 && user.options.states.length === 0) {
+              found = true;
+              console.log("Match found!");
+            } else if (
+              user.options.cities.length > 0 && // User specified cities & lead's city matches specified city
+              user.options.cities.some((city) => {
+                return city.city === chosenLead.city.toLowerCase() && city.state === chosenLead.state.toLowerCase();
+              })
+            ) {
+              found = true;
+              console.log("Match found!");
+            } else if (
+              user.options.states.length > 0 && // User specified states & lead's state matches specified state
+              user.options.states.includes(chosenLead.state.toLowerCase())
+            ) {
+              found = true;
+              console.log("Match found!");
+            }
+          }
+        }
+
+        // const { error } = await supabase
+        //   .from("application")
+        //   .insert({ user_id: user.user_id, receiver_email: chosenLead.email });
+        // if (error) throw error;
+
+        const formattedTemplate = user.template
+          .replace(/{{SENDER_NAME}}/g, user.user.individual.first_name + " " + user.user.individual.last_name)
+          .replace(/{{RECEIVER_NAME}}/g, chosenLead.name)
+          .replace(/{{COMPANY}}/g, chosenLead.organization_name);
+
+        const info = await transporter.sendMail({
+          from: user.user.email,
+          to: chosenLead.email,
+          subject: "Interest in job opening",
+          text: formattedTemplate,
+        });
+        console.log(`Email ${info.messageId} sent!`);
+      } catch (error) {
+        console.error(error);
       }
-
-      const formattedTemplate = user.template
-        .replace(/{{SENDER_NAME}}/g, user.user.individual.first_name + " " + user.user.individual.last_name)
-        .replace(/{{RECEIVER_NAME}}/g, chosenLead.name)
-        .replace(/{{COMPANY}}/g, chosenLead.organization_name);
-
-      const info = await transporter.sendMail({
-        from: user.user.email,
-        to: chosenLead.email,
-        subject: "Interest in job opening",
-        text: formattedTemplate,
-      });
-      console.log(`Email ${info.messageId} sent!`);
     });
 
     return NextResponse.json({ message: "Applications made successfully!" }, { status: 201 });

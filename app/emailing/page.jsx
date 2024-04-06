c16b45e74936390dec5f2b960c9bda40ed9e8a3c
spaaacy <90762160+spaaacy@users.noreply.github.com>
2024-04-05 21:44:47 -0400

    Made modifications to emailing page

--- app/emailing/page.jsx
+++ app/emailing/page.jsx
@@ -9,21 +9,21 @@ import { UserContext } from "@/context/UserContext";
 import Loader from "@/components/common/Loader";
 
 const mockCompanies = [
-  { id: 1, name: "Company 1", state: "State 1", city: "City 1", logo: "/logo.png" },
-  { id: 2, name: "Company 2", state: "State 2", city: "City 2", logo: "/logo.png" },
-  { id: 3, name: "Company 3", state: "State 1", city: "City 3", logo: "/logo.png" },
-  { id: 4, name: "Company 1", state: "State 1", city: "City 1", logo: "/logo.png" },
-  { id: 5, name: "Company 2", state: "State 2", city: "City 2", logo: "/logo.png" },
-  { id: 6, name: "Company 3", state: "State 1", city: "City 3", logo: "/logo.png" },
-  { id: 7, name: "Company 1", state: "State 1", city: "City 1", logo: "/logo.png" },
-  { id: 8, name: "Company 2", state: "State 2", city: "City 2", logo: "/logo.png" },
-  { id: 9, name: "Company 3", state: "State 1", city: "City 3", logo: "/logo.png" },
-  { id: 10, name: "Company 1", state: "State 1", city: "City 1", logo: "/logo.png" },
-  { id: 11, name: "Company 2", state: "State 2", city: "City 2", logo: "/logo.png" },
-  { id: 12, name: "Company 3", state: "State 1", city: "City 3", logo: "/logo.png" },
-  { id: 13, name: "Company 1", state: "State 1", city: "City 1", logo: "/logo.png" },
-  { id: 14, name: "Company 2", state: "State 2", city: "City 2", logo: "/logo.png" },
-  { id: 15, name: "Company 3", state: "State 1", city: "City 3", logo: "/logo.png" },
+  { name: "company 1", logo: "/logo.png" }, // TODO: Logo must be a url
+  { name: "company 2", logo: "/logo.png" },
+  { name: "company 3", logo: "/logo.png" },
+  { name: "company 4", logo: "/logo.png" },
+  { name: "company 5", logo: "/logo.png" },
+  { name: "company 6", logo: "/logo.png" },
+  { name: "company 7", logo: "/logo.png" },
+  { name: "company 8", logo: "/logo.png" },
+  { name: "company 9", logo: "/logo.png" },
+  { name: "company 10", logo: "/logo.png" },
+  { name: "company 11", logo: "/logo.png" },
+  { name: "company 12", logo: "/logo.png" },
+  { name: "company 13", logo: "/logo.png" },
+  { name: "company 14", logo: "/logo.png" },
+  { name: "company 15", logo: "/logo.png" },
 ];
 
 const mockStates = ["State 1", "State 2"];
@@ -35,12 +35,10 @@ const Page = () => {
   const { session } = useContext(UserContext);
   const [companies, setCompanies] = useState([]);
   const [selectedCompanies, setSelectedCompanies] = useState([]);
-  const [emailCount, setEmailCount] = useState(1);
   const [template, setTemplate] = useState({
     subject: "",
     content: "",
   });
-  const [coverLetter, setCoverLetter] = useState("");
   const [states, setStates] = useState([]);
   const [cities, setCities] = useState([]);
   const [filters, setFilters] = useState({
@@ -96,11 +94,11 @@ const Page = () => {
     setFilters({ ...filters, [filterName]: e.target.value });
   };
 
-  const handleCompanySelect = (companyId) => {
-    if (selectedCompanies.includes(companyId)) {
-      setSelectedCompanies(selectedCompanies.filter((id) => id !== companyId));
+  const handleCompanySelect = (name) => {
+    if (selectedCompanies.includes(name)) {
+      setSelectedCompanies(selectedCompanies.filter((selectedName) => selectedName !== name));
     } else {
-      setSelectedCompanies([...selectedCompanies, companyId]);
+      setSelectedCompanies([...selectedCompanies, name]);
     }
   };
 
@@ -110,19 +108,14 @@ const Page = () => {
 
   const handleSubmit = () => {
     console.log("Selected Companies:", selectedCompanies);
-    console.log("Email Count:", emailCount);
     console.log("Template:", template);
-    console.log("Cover Letter:", coverLetter);
     alert("Emails sent successfully!");
   };
 
   let filteredCompanies = [];
   if (companies) {
-    filteredCompanies = companies.filter(
-      (company) =>
-        (filters.state ? company.state === filters.state : true) &&
-        (filters.city ? company.city === filters.city : true) &&
-        (filters.company ? company.name.toLowerCase().includes(filters.company.toLowerCase()) : true)
+    filteredCompanies = companies.filter((company) =>
+      filters.company ? company.name.toLowerCase().includes(filters.company.toLowerCase()) : true
     );
   }
 
@@ -142,55 +135,13 @@ const Page = () => {
       </Head>
       <main className="container mx-auto p-8">
         <section className="text-center mt-2 mb-10">
-          <h2 className="text-4xl font-bold text-white">Email Service</h2>
+          <h2 className="text-3xl font-bold text-white">Preferences</h2>
         </section>
-        <div className="bg-gray-800 p-6 rounded-lg shadow-lg mb-8 w-full">
-          <h3 className="text-2xl font-bold mb-4 flex items-center text-purple-400">
-            <FaFilter className="mr-2" />
-            Filters
-          </h3>
-          <div className="grid grid-cols-1 sm:grid-cols-3 gap-4">
-            <div>
-              <label htmlFor="state" className="block text-lg font-semibold mb-1">
-                State:
-              </label>
-              <select
-                id="state"
-                value={filters.state}
-                onChange={(e) => handleFilterChange(e, "state")}
-                className="w-full p-2 bg-gray-700 text-white rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-500"
-              >
-                <option value="">All States</option>
-                {states.map((state) => (
-                  <option key={state} value={state}>
-                    {state}
-                  </option>
-                ))}
-              </select>
-            </div>
-            <div>
-              <label htmlFor="city" className="block text-lg font-semibold mb-1">
-                City:
-              </label>
-              <select
-                id="city"
-                value={filters.city}
-                onChange={(e) => handleFilterChange(e, "city")}
-                className="w-full p-2 bg-gray-700 text-white rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-500"
-              >
-                <option value="">All Cities</option>
-                {cities.map((city) => (
-                  <option key={city} value={city}>
-                    {city}
-                  </option>
-                ))}
-              </select>
-            </div>
-            <div>
-              <label htmlFor="company" className="block text-lg font-semibold mb-1">
-                Company:
-              </label>
-              <div className="relative">
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 w-[126rem]">
+          <div className="lg:col-span-2">
+            <div className="bg-gray-800 p-6 rounded-lg shadow-lg mb-8 w-full">
+              <h3 className="text-2xl font-bold mb-4 text-purple-400">Companies</h3>
+              <div className="relative my-4">
                 <input
                   id="company"
                   value={filters.company}
@@ -201,32 +152,22 @@ const Page = () => {
                 />
                 <FaSearch className="absolute top-1/2 left-3 transform -translate-y-1/2 text-purple-400" />
               </div>
-            </div>
-          </div>
-        </div>
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 w-[126rem]">
-          <div className="lg:col-span-2">
-            <div className="bg-gray-800 p-6 rounded-lg shadow-lg mb-8 w-full">
-              <h3 className="text-2xl font-bold mb-4 text-purple-400">Companies</h3>
               <div className="max-h-96 overflow-auto">
                 <div className="grid grid-cols-1 sm:grid-cols-3 gap-6">
                   {filteredCompanies.map((company) => (
                     <label
-                      key={company.id}
+                      key={company.name}
                       className="flex items-center bg-gray-700 p-4 rounded-lg shadow-md cursor-pointer transition-colors duration-300 hover:bg-gray-600"
                     >
                       <input
                         type="checkbox"
-                        checked={selectedCompanies.includes(company.id)}
-                        onChange={() => handleCompanySelect(company.id)}
-                        className="mr-4 h-5 w-5 text-purple-500 focus:ring-purple-500 rounded"
+                        checked={selectedCompanies.includes(company.name)}
+                        onChange={() => handleCompanySelect(company.name)}
+                        className="capitalize mr-4 h-5 w-5 text-purple-500 focus:ring-purple-500 rounded"
                       />
                       <img src={company.logo} alt={company.name} className="h-10 w-10 rounded-full mr-4" />
                       <div>
-                        <h4 className="text-lg font-semibold">{company.name}</h4>
-                        <p className="text-sm text-gray-400">
-                          {company.state}, {company.city}
-                        </p>
+                        <h4 className="capitalize text-lg font-semibold">{company.name}</h4>
                       </div>
                     </label>
                   ))}
@@ -237,30 +178,19 @@ const Page = () => {
               <section className="bg-gray-800 p-6 rounded-lg shadow-lg mb-8 w-full">
                 <div className="flex items-center justify-between mb-6">
                   <h3 className="text-2xl font-bold text-purple-400">Selected Companies</h3>
-                  <div className="flex items-center space-x-4">
-                    <span className="text-lg">Emails per Company:</span>
-                    <input
-                      type="number"
-                      value={emailCount}
-                      onChange={(e) => setEmailCount(parseInt(e.target.value))}
-                      className="w-24 p-2 bg-gray-700 text-white rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-500"
-                      min={1}
-                    />
-                    <span className="text-lg">Total Emails: {emailCount * selectedCompanies.length}</span>
-                  </div>
                 </div>
-                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-                  {selectedCompanies.map((companyId) => {
-                    const company = companies.find((c) => c.id === companyId);
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6">
+                  {selectedCompanies.map((companyName) => {
+                    const company = companies.find((c) => c.name === companyName);
                     return (
-                      <div key={companyId} className="flex items-center">
+                      <div key={companyName} className="flex items-center">
                         <img src={company.logo} alt={company.name} className="h-10 w-10 rounded-full mr-4" />
                         <span className="text-lg">{company.name}</span>
                         <button
-                          onClick={() => handleCompanySelect(companyId)}
-                          className="ml-auto bg-red-600 hover:bg-red-700 text-white font-semibold rounded-lg px-2 py-1 focus:outline-none focus:ring-2 focus:ring-red-500"
+                          onClick={() => handleCompanySelect(companyName)}
+                          className="ml-auto text-white font-semibold rounded-lg px-2 py-1 focus:outline-none focus:ring-2 focus:ring-red-500"
                         >
-                          <FaTrash />
+                          <FaTrash color="#DC1C1C" />
                         </button>
                       </div>
                     );
@@ -268,6 +198,47 @@ const Page = () => {
                 </div>
               </section>
             )}
+            <div className="bg-gray-800 p-6 rounded-lg shadow-lg mb-8 w-full">
+              <h3 className="text-2xl font-bold mb-4 flex items-center text-purple-400">Locations</h3>
+              <div className="grid grid-cols-1 sm:grid-cols-3 gap-4">
+                <div>
+                  <label htmlFor="state" className="block text-lg font-semibold mb-1">
+                    State:
+                  </label>
+                  <select
+                    id="state"
+                    value={filters.state}
+                    onChange={(e) => handleFilterChange(e, "state")}
+                    className="w-full p-2 bg-gray-700 text-white rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-500"
+                  >
+                    <option value="">All States</option>
+                    {states.map((state) => (
+                      <option key={state} value={state}>
+                        {state}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+                <div>
+                  <label htmlFor="city" className="block text-lg font-semibold mb-1">
+                    City:
+                  </label>
+                  <select
+                    id="city"
+                    value={filters.city}
+                    onChange={(e) => handleFilterChange(e, "city")}
+                    className="w-full p-2 bg-gray-700 text-white rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-500"
+                  >
+                    <option value="">All Cities</option>
+                    {cities.map((city) => (
+                      <option key={city} value={city}>
+                        {city}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+              </div>
+            </div>
             <section className="bg-gray-800 p-6 rounded-lg shadow-lg mb-8 w-full">
               <div className="flex items-center justify-between mb-6">
                 <h3 className="text-2xl font-bold text-purple-400">Email Template</h3>
@@ -318,25 +289,14 @@ const Page = () => {
                 </div>
               )}
             </section>
-            <section className="bg-gray-800 p-6 rounded-lg shadow-lg w-full">
-              <h3 className="text-2xl font-bold mb-4 text-purple-400">Cover Letter</h3>
-              <textarea
-                value={coverLetter}
-                onChange={(e) => setCoverLetter(e.target.value)}
-                className="w-full h-48 p-2 bg-gray-700 text-white rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-500"
-              ></textarea>
-            </section>
           </div>
         </div>
-        <div className="mt-12 text-right">
-          <button
-            onClick={handleSubmit}
-            className="px-6 py-3 bg-purple-600 text-white text-lg font-semibold rounded-lg hover:bg-purple-700 transition-colors duration-300 flex items-center"
-          >
-            <FaPaperPlane className="mr-2" />
-            Send Emails
-          </button>
-        </div>
+        <button
+          onClick={handleSubmit}
+          className="px-6 py-3 ml-auto bg-purple-600 text-white text-lg font-semibold rounded-lg hover:bg-purple-700 transition-colors duration-300 flex items-center"
+        >
+          Save
+        </button>
       </main>
       <Footer />
     </main>

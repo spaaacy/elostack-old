af50e2c3701cbfd49c9178761446c254248cef9e
spaaacy <90762160+spaaacy@users.noreply.github.com>
2024-04-07 20:47:31 -0400

    Remove btn for purchase

--- app/emailing/page.jsx
+++ app/emailing/page.jsx
@@ -513,15 +513,6 @@ const Emailing = () => {
             Save Preferences
           </button>
         ))}
-      {user?.credits ===
-        0(
-          <Link
-            href={"/plans"}
-            className="ml-auto px-6 py-3 bg-purple-600 text-white text-lg font-semibold rounded-lg hover:bg-purple-700 transition-colors duration-300 flex items-center"
-          >
-            Purchase credits
-          </Link>
-        )}
     </main>
   );
 };

18cdc113dc06f2fc9aa59e33601fadd2783ac629
spaaacy <90762160+spaaacy@users.noreply.github.com>
2024-04-23 08:18:03 -0400

    Changed to linkedin node package

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,8 +2,8 @@
 import GoogleAnalytics from "@/components/common/GoogleAnalytics";
 import { UserProvider } from "@/context/UserContext";
 import "@/styles/globals.css";
-import Script from "next/script";
 import { usePathname } from "next/navigation";
+import { LinkedInInsightTag } from "nextjs-linkedin-insight-tag";
 
 const getTitle = (pathname) => {
   if (pathname.startsWith("/emailing")) {
@@ -40,37 +40,7 @@ export default function RootLayout({ children }) {
       </head>
       <body className="flex flex-col min-h-screen bg-[#0f0f1c]">
         <UserProvider>{children}</UserProvider>
-
-        <script type="text/javascript">
-          _linkedin_partner_id = "7061681"; window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
-          window._linkedin_data_partner_ids.push(_linkedin_partner_id);
-        </script>
-        <script type="text/javascript">
-          {function (l) {
-            if (!l) {
-              window.lintrk = function (a, b) {
-                window.lintrk.q.push([a, b]);
-              };
-              window.lintrk.q = [];
-            }
-            var s = document.getElementsByTagName("script")[0];
-            var b = document.createElement("script");
-            b.type = "text/javascript";
-            b.async = true;
-            b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
-            s.parentNode.insertBefore(b, s);
-          }}
-          (window.lintrk);
-        </script>
-        <noscript>
-          <img
-            height="1"
-            width="1"
-            style="display:none;"
-            alt=""
-            src="https://px.ads.linkedin.com/collect/?pid=7061681&fmt=gif"
-          />
-        </noscript>
+        <LinkedInInsightTag />
       </body>
     </html>
   );

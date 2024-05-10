f4681cee66daddb220e9891cf8f03628cd9960b0
spaaacy <90762160+spaaacy@users.noreply.github.com>
2024-05-10 08:12:09 -0400

    Added free tier to send-email

--- supabase/functions/send-email/index.ts
+++ supabase/functions/send-email/index.ts
@@ -12,6 +12,7 @@ Deno.serve(async (req, res) => {
   }
 
   const authHeader = req.headers.get("authorization");
+  const freeTier = req.headers.get("free-tier");
   if (authHeader && authHeader.startsWith("Bearer ")) {
     if (authHeader.split(" ")[1] !== Deno.env.get("CRON_SECRET")) {
       return new Response("Unauthorized!", { status: 401 });
@@ -32,16 +33,16 @@ Deno.serve(async (req, res) => {
 
     console.log("Beginning email loop...");
     for (const user of subscriber.data) {
-      console.log("Going to next user...");
+      console.log(`Going to user ${user.user_id}`);
       try {
         // Check if user's subscription is active
         if (!user.active) continue;
+
         // Check if user's access & refresh token are available
         const dateNow = Date.now();
         const date = new Date(dateNow);
         const gmt = date.toLocaleString("en-US", { timeZone: "GMT" });
         const gmtDate = new Date(gmt);
-
         if (
           !user.refresh_token ||
           (!user.refresh_token && user.access_token && Date.parse(user.oauth_expiry_date) < gmtDate.getTime())
@@ -59,133 +60,104 @@ Deno.serve(async (req, res) => {
           throw Error("Refresh and access token not found!");
         }
 
+        if ((user.user.credits > 0 && freeTier === "true") || (freeTier === "false" && user.user.credits <= 0))
+          continue;
+
+        // Find lead
         let chosenLead;
-        if (user.selected_leads) {
-          if (user.leads_chosen.leads.length > 0) {
-            chosenLead = await supabase.rpc("select_lead_from_array", {
-              subscriber_user_id: user.user_id,
-              lead_ids: user.leads_chosen.leads,
-            });
-            if (chosenLead.error) throw chosenLead.error;
-          } else {
-            const { error } = await supabase.rpc("toggle_subscriber_active", {
-              subscriber_user_id: user.user_id,
-              new_status: false,
-            });
-            if (error) throw error;
-            continue;
-          }
-        } else {
-          chosenLead = await supabase.rpc("find_matching_lead", {
-            subscriber_user_id: user.user_id,
-            states: user.options.states,
-            companies: user.options.companies,
-            seniorities: user.options.seniorities,
-          });
-          if (chosenLead.error) throw chosenLead.error;
-        }
+        chosenLead = await supabase.rpc("find_matching_lead", {
+          subscriber_user_id: user.user_id,
+          states: user.options.states,
+          companies: user.options.companies,
+          seniorities: user.options.seniorities,
+        });
+        if (chosenLead.error) throw chosenLead.error;
         chosenLead = chosenLead.data[0];
 
         // If lead is found start emailing process
         if (chosenLead) {
-          // Fetch attachments
-          let results = await supabase.storage.from("attachments").list(user.user_id, {
-            limit: 100,
-            offset: 0,
-            sortBy: { column: "name", order: "asc" },
-          });
-          if (results.error) throw results.error;
+          try {
+            // Fetch attachments
+            let results = await supabase.storage.from("attachments").list(user.user_id, {
+              limit: 100,
+              offset: 0,
+              sortBy: { column: "name", order: "asc" },
+            });
+            if (results.error) throw results.error;
 
-          // Fetch attachments and store as Buffer in attachments[]
-          const attachments = [];
-          for (let attachment of results.data) {
-            results = await supabase.storage.from("attachments").download(`${user.user_id}/${attachment.name}`);
-            const arrayBuffer = await results.data.arrayBuffer();
-            const buffer = Buffer.from(arrayBuffer);
-            attachments.push({ filename: attachment.name, content: buffer });
-          }
+            // Store as Buffer in attachments[]
+            const attachments = [];
+            for (let attachment of results.data) {
+              results = await supabase.storage.from("attachments").download(`${user.user_id}/${attachment.name}`);
+              const arrayBuffer = await results.data.arrayBuffer();
+              const buffer = Buffer.from(arrayBuffer);
+              attachments.push({ filename: attachment.name, content: buffer });
+            }
 
-          // Increment the send count if the record does exist
-          results = await supabase.rpc("increment_monthly_send_count", { lead_id: chosenLead.id });
-          if (results.error) throw results.error;
-          if (!results.data) {
-            console.error("Lead has already met their monthly limit!");
-            continue;
-          }
-          console.log("Lead monthly send count incremented!");
+            // Format the email subject/body
+            const formattedTemplate = user.email_body
+              .replace(/{{LEAD_FIRST_NAME}}/g, chosenLead.first_name)
+              .replace(/{{LEAD_LAST_NAME}}/g, chosenLead.last_name)
+              .replace(/{{COMPANY}}/g, chosenLead.organization_name);
+            const formattedSubject = user.email_subject
+              .replace(/{{LEAD_FIRST_NAME}}/g, chosenLead.first_name)
+              .replace(/{{LEAD_LAST_NAME}}/g, chosenLead.last_name)
+              .replace(/{{COMPANY}}/g, chosenLead.organization_name);
 
-          // Check if user has sufficient credits and decrement if true, otherwise go to next user
-          results = await supabase.rpc("decrement_subscriber_credits", {
-            subscriber_user_id: user.user_id,
-          });
-          if (results.error) throw results.error;
-          if (!results.data) {
-            console.error("Insufficient credits!");
-            continue;
-          }
-          console.log("Credits decremented successfully!");
+            const transporter = nodeMailer.createTransport({
+              service: "Gmail",
+              host: "smtp.gmail.com",
+              port: 465,
+              secure: true,
+              auth: {
+                type: "OAuth2",
+                clientId: Deno.env.get("GOOGLE_OAUTH_CLIENT_ID"),
+                clientSecret: Deno.env.get("GOOGLE_OAUTH_CLIENT_SECRET"),
+                user: user.user.email,
+                refreshToken: user.refresh_token,
+                accessToken: user.access_token,
+              },
+            });
 
-          // Create a record of the application if email succeeds
-          results = await supabase.from("application").insert({ user_id: user.user_id, lead_id: chosenLead.id });
-          if (results.error) throw results.error;
-          console.log("Application record created!");
-
-          // Format the email subject/body
-          const formattedTemplate = user.email_body
-            .replace(/{{LEAD_FIRST_NAME}}/g, chosenLead.first_name)
-            .replace(/{{LEAD_LAST_NAME}}/g, chosenLead.last_name)
-            .replace(/{{COMPANY}}/g, chosenLead.organization_name);
-          const formattedSubject = user.email_subject
-            .replace(/{{LEAD_FIRST_NAME}}/g, chosenLead.first_name)
-            .replace(/{{LEAD_LAST_NAME}}/g, chosenLead.last_name)
-            .replace(/{{COMPANY}}/g, chosenLead.organization_name);
-
-          const transporter = nodeMailer.createTransport({
-            service: "Gmail",
-            host: "smtp.gmail.com",
-            port: 465,
-            secure: true,
-            auth: {
-              type: "OAuth2",
-              clientId: Deno.env.get("GOOGLE_OAUTH_CLIENT_ID"),
-              clientSecret: Deno.env.get("GOOGLE_OAUTH_CLIENT_SECRET"),
-              user: user.user.email,
-              refreshToken: user.refresh_token,
-              accessToken: user.access_token,
-            },
-          });
-
-          // Send out email
-          await transporter.sendMail(
-            {
+            // Send out email
+            const info = await transporter.sendMail({
               from: user.user.email,
               to: chosenLead.email,
               subject: formattedSubject,
               text: formattedTemplate,
               attachments,
-            },
-            (error, info) => {
-              if (error) console.error(error);
-              else console.log(`Email sent: ${info.messageId}`);
-            }
-          );
+            });
 
-          // Remove lead from chosen_leads if selected manually
-          if (user.selected_leads) {
-            const newChosenLeads = user.leads_chosen.leads.filter((lead) => lead !== chosenLead.id);
-            results = await supabase
-              .from("subscriber")
-              .update({ leads_chosen: { leads: newChosenLeads } })
-              .eq("user_id", user.user_id);
+            console.log(`Email ${info.messageId} sent!`);
+
+            // Increment the send count if the record does exist
+            results = await supabase.rpc("increment_monthly_send_count", { lead_id: chosenLead.id });
             if (results.error) throw results.error;
-
-            if (newChosenLeads.length === 0) {
-              const { error } = await supabase.rpc("toggle_subscriber_active", {
-                subscriber_user_id: user.user_id,
-                new_status: false,
-              });
-              if (error) throw error;
+            if (!results.data) {
+              console.error("Lead has already met their monthly limit!");
+              continue;
             }
+            console.log("Lead monthly send count incremented!");
+
+            // Check if user has sufficient credits and decrement if true, otherwise go to next user
+            if (!freeTier) {
+              results = await supabase.rpc("decrement_subscriber_credits", {
+                subscriber_user_id: user.user_id,
+              });
+              if (results.error) throw results.error;
+              if (!results.data) {
+                console.error("Insufficient credits!");
+                continue;
+              }
+              console.log("Credits decremented successfully!");
+            }
+
+            // Create a record of the application if email succeeds
+            results = await supabase.from("application").insert({ user_id: user.user_id, lead_id: chosenLead.id });
+            if (results.error) throw results.error;
+            console.log("Application record created!");
+          } catch (error) {
+            console.error(error);
           }
         } else {
           console.log("No lead was found!");
